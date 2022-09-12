f0eb03b40481cdcf576ac51a8a0ae0f36bbaea9c
trungngvu <vutrungnghiahn@gmail.com>
2022-09-12 16:22:45 +0700

    cuu son

--- src/components/Layout/DefaultLayout/Footer/footer.js
+++ src/components/Layout/DefaultLayout/Footer/footer.js
@@ -1,5 +1,49 @@
 function Footer() {
-  return <h2>Footer</h2>;
+    return (
+        <div class="footer">
+            <div class="footer_app">
+                <div class="logo">
+                    <div class="logo_main">Logo</div>
+                    <div class="logo_list">
+                        <div class="logo_list-content">
+                            <p>Copyright 2022 Super-Heroes Shop</p>
+                        </div>
+                        <ul class="logo_list-item">
+                            <li class="logo_list-item-icon">
+                                <i class="fa-brands fa-instagram"></i>
+                            </li>
+                            <li class="logo_list-item-icon">
+                                <i class="fa-brands fa-twitter"></i>
+                            </li>
+                            <li class="logo_list-item-icon">
+                                <i class="fa-brands fa-facebook"></i>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="list">
+                    <ul class="list-item">
+                        <li class="list-item_infor list-item_infor1">Menu</li>
+                        <li class="list-item_infor">Home</li>
+                        <li class="list-item_infor">News</li>
+                        <li class="list-item_infor">Characters</li>
+                    </ul>
+                    <ul class="list-item">
+                        <li class="list-item_infor list-item_infor1">Company</li>
+                        <li class="list-item_infor">Managenment</li>
+                        <li class="list-item_infor">Out story</li>
+                        <li class="list-item_infor">Career</li>
+                    </ul>
+                    <ul class="list-item">
+                        <li class="list-item_infor list-item_infor1">Support</li>
+                        <li class="list-item_infor">Support Center</li>
+                        <li class="list-item_infor">Transaction</li>
+                        <li class="list-item_infor">Contact us</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default Footer;
